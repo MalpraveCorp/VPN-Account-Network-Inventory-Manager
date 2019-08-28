a4f96f515549cca61a8a65ab4502aeca7ac73d0c
Pedro <pedrogomesyoo@gmail.com>
2019-08-29 01:52:53 +0700

    address linting issues

--- vpn_account.py
+++ vpn_account.py
@@ -19,17 +19,17 @@ def get_octets(address):
     return str(address).split(".")
 
 def printer(rows):
-    if args["json"]:
+    if ARGS["json"]:
         print(json.dumps(rows, indent=4))
     else:
         for row in rows:
             print(row)
 
-def dict_factory(cur, row):
-    d = {}
-    for idx, col in enumerate(cur.description):
-        d[col[0]] = row[idx]
-    return d
+def dict_factory(cursor, row):
+    dic = {}
+    for idx, col in enumerate(cursor.description):
+        dic[col[0]] = row[idx]
+    return dic
 
 # def get_ippools_usage(conn):
 #     return {
@@ -64,42 +64,44 @@ def get_arguments():
     # parser.print_help()
     return vars(parsed_args)
 
-# generate /20 IP Pool in provided subnet
 def get_pools(subnet, name):
+    """ Generates list of IP pairs on a /20 available for the given subnet."""
     subnet = int(subnet)
     ret = []
-    for thirdOctete in range(subnet, subnet+16):
+    for third_octete in range(subnet, subnet+16):
         # each client needs 4 ips - must be divisible by 4
-        for forthOctete in get_chunks(range(1, 253), 4):
+        for forth_octete in get_chunks(range(1, 253), 4):
             # Allows for 0.0/20 0.16/20 0.32/20
-            network = DEFAULT_NETWORK + str(thirdOctete)
-            ret.append([network + '.0', name, forthOctete[0], forthOctete[1], network + '.255', 0])
+            network = DEFAULT_NETWORK + str(third_octete)
+            ret.append([network + '.0', name, forth_octete[0], forth_octete[1], network + '.255', 0])
     return ret
 
 def get_accounts():
+    """ Get all user accounts """
     sql = 'SELECT * FROM accounts'
-    cur.execute(sql)
-    return cur.fetchall()
+    CUR.execute(sql)
+    return CUR.fetchall()
 
 def get_accounts_full():
+    """ Get all user accounts along with respective ip addresses assigned """
     sql = 'SELECT * FROM accounts LEFT JOIN ippools on accounts.ippool_id = ippools.id'
-    cur.execute(sql)
-    return cur.fetchall()
+    CUR.execute(sql)
+    return CUR.fetchall()
 
 def get_ippools():
+    """ Get all IP pools"""
     sql = 'SELECT * FROM ippools'
-    cur.execute(sql)
-    return cur.fetchall()
+    CUR.execute(sql)
+    return CUR.fetchall()
 
 def get_database():
-    conn = sqlite3.connect('vpnaccount.db')
-    conn.row_factory = dict_factory
-    return conn
+    """ Return instance of database connection"""
+    connection = sqlite3.connect('vpnaccount.db')
+    connection.row_factory = dict_factory
+    return connection
 
-def database_initialized():
-    return True
-
-def create_tables(conn):
+def create_tables():
+    """ Initialize database with new tables """
     sql = """CREATE TABLE IF NOT EXISTS accounts
                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
                 username VARCHAR(100) NOT NULL,
@@ -108,7 +110,7 @@ def create_tables(conn):
                 ippool_id INTEGER,
                 CONSTRAINT username_unique UNIQUE (username)
                 )"""
-    conn.cursor().execute(sql)
+    CONN.cursor().execute(sql)
     sql = """CREATE TABLE IF NOT EXISTS ippools
                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
                 network VARCHAR(100) NOT NULL,
@@ -120,97 +122,108 @@ def create_tables(conn):
                 CONSTRAINT network_address UNIQUE (network, first_ip, last_ip)
                 )"""
 
-    conn.cursor().execute(sql)
-    conn.commit()
+    CONN.cursor().execute(sql)
+    CONN.commit()
 
-def get_available_ip(conn):
-    c = conn.cursor()
-    c.execute('SELECT * FROM ippools WHERE status = 0')
-    return c.fetchone()
+def get_available_ip():
+    """ Returns the first IP pair available """
+    cur = CONN.cursor()
+    cur.execute('SELECT * FROM ippools WHERE status = 0')
+    return cur.fetchone()
 
-def get_ippool_id(conn, first_ip, last_ip, network, subnet):
-    c = conn.cursor()
-    c.execute('SELECT * FROM ippools WHERE network = ? AND first_ip = ? AND last_ip = ? and subnet LIKE ?', [network, first_ip, last_ip, subnet])
-    return c.fetchone()
+def get_ippool_id(first_ip, last_ip, network, subnet):
+    """ Returns IP pool id for given input parameters"""
+    cur = CONN.cursor()
+    cur.execute('SELECT * FROM ippools WHERE network = ? AND first_ip = ? AND last_ip = ? and subnet LIKE ?', [network, first_ip, last_ip, subnet])
+    return cur.fetchone()
 
-def create_ippools(conn, subnet=DEFAULT_SUBNET, name=DEFAULT_GROUP):
-    conn.cursor().executemany('INSERT INTO ippools(network, subnet, first_ip, last_ip, broadcast, status) VALUES (?,?,?,?,?,?)', get_pools(subnet, name))
-    conn.commit()
+def create_ippools(subnet=DEFAULT_SUBNET, name=DEFAULT_GROUP):
+    """ Populates IP pools database """
+    CONN.cursor().executemany('INSERT INTO ippools(network, subnet, first_ip, last_ip, broadcast, status) VALUES (?,?,?,?,?,?)', get_pools(subnet, name))
+    CONN.commit()
 
 def create_user(email):
-    ip_address_id = get_available_ip(conn)['id']
-    conn.cursor().execute('UPDATE ippools SET status = 1 WHERE id = ?', (ip_address_id,))
-    conn.cursor().execute('INSERT INTO accounts(username, email, status, ippool_id) VALUES (?,?,?,?)', [email, email, 1, ip_address_id])
-    conn.commit()
+    """ Create a new user and assign it an IP pair """
+    ip_address_id = get_available_ip()['id']
+    CONN.cursor().execute('UPDATE ippools SET status = 1 WHERE id = ?', (ip_address_id,))
+    CONN.cursor().execute('INSERT INTO accounts(username, email, status, ippool_id) VALUES (?,?,?,?)', [email, email, 1, ip_address_id])
+    CONN.commit()
 
 def create_user_hardcoded(email, first_ip, last_ip, subnet):
+    """ Create a new user from existing system, preserving ip pairs """
     network = DEFAULT_NETWORK + get_octets(first_ip)[2] + '.0'
     first_ip = get_octets(first_ip)[3]
     last_ip = get_octets(last_ip)[3]
     print(first_ip, last_ip, network, subnet)
-    ip_address_id = get_ippool_id(conn, first_ip, last_ip, network, subnet)['id']
-    conn.cursor().execute('UPDATE ippools SET status = 1 WHERE id = ?', (ip_address_id,))
-    conn.cursor().execute('INSERT INTO accounts(username, email, status, ippool_id) VALUES (?,?,?,?)', [email, email, 1, ip_address_id])
-    conn.commit()
+    ip_address_id = get_ippool_id(first_ip, last_ip, network, subnet)['id']
+    CONN.cursor().execute('UPDATE ippools SET status = 1 WHERE id = ?', (ip_address_id,))
+    CONN.cursor().execute('INSERT INTO accounts(username, email, status, ippool_id) VALUES (?,?,?,?)', [email, email, 1, ip_address_id])
+    CONN.commit()
 
 def revoke_user(email):
-    c = conn.cursor()
-    c.execute('SELECT * FROM accounts WHERE username LIKE ?', (email,))
-    account = c.fetchone()
-    c.execute('UPDATE accounts SET status = 0 WHERE id = ?', (int(account['id']), )) # also remove ippool_id?
-    c.execute('UPDATE ippools SET status = 0 WHERE id = ?', (int(account['ippool_id']), ))
-    conn.commit()
+    """ Disable user and free its IP pair """
+    cur = CONN.cursor()
+    cur.execute('SELECT * FROM accounts WHERE username LIKE ?', (email,))
+    account = cur.fetchone()
+    cur.execute('UPDATE accounts SET status = 0 WHERE id = ?', (int(account['id']), )) # also remove ippool_id?
+    cur.execute('UPDATE ippools SET status = 0 WHERE id = ?', (int(account['ippool_id']), ))
+    CONN.commit()
 
-def handler_create_pool(args):
-    if len(args) == 2:
+def handler_create_pool(arguments):
+    if len(arguments) == 2:
         print('Creating subnet with provided name and range.')
-        create_ippools(conn, args[0], args[1])
-    elif len(args) == 1:
+        create_ippools(arguments[0], arguments[1])
+    elif len(arguments) == 1:
         print('You should provide no arguments for defaults or subnet and group name e.g. --create-pool 16 prod')
     else:
         print('Creating default subnet.')
-        create_ippools(conn)
+        create_ippools()
 
-def handler_create_user_hardcoded():
-    if len(args) == 4:
-        create_user_hardcoded(conn, args[0], args[2], args[1], args[3])
-    else:
-        print(get_last_octet(args[2]))
+def handler_create_user_hardcoded(arguments):
+    if len(arguments) != 4:
         print('You need to specify 4 params: EMAIL FISTIP LASTIP SUBNET')
+        return
+
+    email = arguments[0]
+    first_ip = arguments[1]
+    last_ip = arguments[2]
+    subnet = arguments[3]
+
+    create_user_hardcoded(email, first_ip, last_ip, subnet)
 
 
 #
 # main
 #
 
-conn = get_database()
+CONN = get_database()
 
 # if not database_initialized:
-create_tables(conn)
+create_tables()
 
-cur = conn.cursor()
-args = get_arguments()
+CUR = CONN.cursor()
+ARGS = get_arguments()
 
-if args["show_accounts"]:
+if ARGS["show_accounts"]:
     printer(get_accounts())
 
-if args["show_accounts_full"]:
+if ARGS["show_accounts_full"]:
     printer(get_accounts_full())
 
-if args["show_pools"]:
+if ARGS["show_pools"]:
     printer(get_ippools())
 
-if args["create_account"] is not None:
-    create_user(args["create_account"])
+if ARGS["create_account"] is not None:
+    create_user(ARGS["create_account"])
 
-if args["create_account_hardcoded"] is not None:
-    handler_create_user_hardcoded(args["create_account_hardcoded"])
+if ARGS["create_account_hardcoded"] is not None:
+    handler_create_user_hardcoded(ARGS["create_account_hardcoded"])
 
-if args["revoke_account"] is not None:
-    revoke_user(args["revoke_account"])
+if ARGS["revoke_account"] is not None:
+    revoke_user(ARGS["revoke_account"])
 
-if args["create_pool"] is not None:
-    handler_create_pool(args["create_pool"])
+if ARGS["create_pool"] is not None:
+    handler_create_pool(ARGS["create_pool"])
 
 
-conn.close()
+CONN.close()
